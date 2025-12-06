7e01a6f0fe5f4b0d4044c5f79e8643f9557a9835
MamedliFidan <mfidan742@gmail.com>
2025-12-06 01:16:19 -0800

    updated

--- app.js
+++ app.js
@@ -1,17 +1,125 @@
 //task1
-let a = prompt("4 reqemli eded");
-
-if (a >= 1000 && a < 10000) {
-    let a1 = Math.floor(a / 1000);
-    let a2 = Math.floor((a % 1000) / 100);
-    let a3 = Math.floor((a % 100) / 10);
-    let a4 = a % 10;
-
-    console.log(a1 + a2 + a3 + a4);
-} else {
-    console.log("eded 4 reqemli deyil");
+let data = prompt("Enter some data: YYYY.MM.DD");
+let year = 0;
+let month = 0;
+let day = 0;
+switch (data.slice(0, 4)) { //2020.06.05
+    case "2020":
+        year = 2020;
+        break;
+    case '2021':
+        year = 2021;
+        break;
+    case '2022':
+        year = 2022;
+        break; 
+    
+    case '2023':
+        year = 2023;
+        break;
+        
+    case '2024':
+        year = 2024;
+        break;
+    case '2025':
+        year = 2025;
+        break;
 }
+switch (data.slice(5, 7)) {
+    case '01':
+        month = "yanuary";
+        break;
+    case '02':
+        month = "february";
+        break;
+    case '03':
+        month = "march";
+        break;
+    case '04':
+        month = "april";
+        break;
+    case '05':
+        month = "may";
+        break;
+    case '06':
+        month = "june";
+        break;
+    case '07':
+        month = "july";
+        break;
+    case '08':
+        month = "august";
+        break;
+    case '09':
+        month = "september";
+        break;
+    case '10':
+        month = "october";
+        break;
+    case '11':
+        month = "november";
+        break;
+    case '12':
+        month = "december";
+        break;
+}
+switch (data.slice(8, 10)) {
+    case '01':
+        day = 1;
+        break;
+    case '02':
+        day = 2;
+        break;
+    case '03':
+        day = 3;
+        break;
+    case '04':
+        day = 4;
+        break;
+    case '05':
+        day = 5;
+        break;
+}
+console.log(day+" "+month+ " " +year+"-cu il");
+
 //task2
-let name=prompt("adiniz nedir?");
-let surname =prompt("soyadiniz nedir?");
-console.log(`Salam , ${name}`,`${surname} !`);
+let month1 = 0
+switch (month1) {
+    case '1':
+        month = "yanuary";
+        break;
+    case '2':
+        month = "february";
+        break;
+    case '3':
+        month = "march";
+        break;
+    case '4':
+        month = "april";
+        break;
+    case '5':
+        month = "may";
+        break;
+    case '6':
+        month = "june";
+        break;
+    case '7':
+        month = "july";
+        break;
+    case '8':
+        month = "august";
+        break;
+    case '9':
+        month = "september";
+        break;
+    case '10':
+        month = "october";
+        break;
+    case '11':
+        month = "november";
+        break;
+    case '12':
+        month = "december";
+        break;
+}
+console.log(month1);
