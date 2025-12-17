ad69927ddd84c9f4c7b757e619acd7ddfe755424
MamedliFidan <mfidan742@gmail.com>
2025-12-17 18:25:19 +0400

    updated

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-let cash = 2;
-function change(rubles,dollars=cash){
-    rubles=dollars*78;
-    console.log(rubles);
+//task3
+let i=100;
+while (i<1000) {
+  console.log(i);
+  i+=10;
+}
+
+//diqər üsulla 
+
+let b = 1;
+while (b<1000){
+  b+=1;
+  if (b%10==0){
+    console.log(b);
+  }
 } 
-change(cash);
+
+//task4
+let a=10;
+let sum=0;
+while (a<=100){
+  if (a%2!=0){
+    sum+=(a%10+a/10);
+    console.log(sum);
+  }
+  a++;
+  
+}
