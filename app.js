3f928ff16b41e68bcf5bac0e06f80419ce1699d4
MamedliFidan <mfidan742@gmail.com>
2026-01-14 15:57:37 +0400

    hm

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-let arr=[1,2,3,4,5,7,9];
-let sum=0;
-for(let i=0;i<arr.length;i++){
-    if(arr[i]%2!==0){
-        sum+=arr[i];
-    }
+let obj = {
+  name: "John",
+  age() {
+    return Number(prompt("Enter your age:"));
+  },
+  agePension() {
+    return Number(65 - this.age());
+  }
 }
-console.log("Sum of odd numbers:",sum);
 
-let arr2=["js","front","arrays","html","css"];
-let arr3=[];
-for(let i=0;i<arr2.length;i++){
-  let word = "";
-  for(let j =arr2[i].length-1 ;j>=0;j--){
-    word +=arr2[i][j];}
-  arr3.push(word);
-}
-console.log("Reversed strings array:",arr3);
+console.log(obj.agePension());
